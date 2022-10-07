bf1975b788645e1a094f84703a5f5c23ea337615
Joshua <jy87@canterbury.ac.uk>
2022-10-07 13:11:24 +0000

    Html Hello Page, Started framing the API requests

--- server/main.py
+++ server/main.py
@@ -1,12 +1,36 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+posts = []
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse,status_code=200)
 async def root():
-    return ("Hello world")
+    return """
+    <html>
+        <head>
+            <title>I'm Sorry I think You're Lost....</title>
+        </head>
+        <body>
+            <h1>I'm Sorry I think You're Lost....</h1>
+            <h2>This is not the page you are looking for</h2>
+        </body>
+    </html>
+    """
 
-@app.get("/item/")
-async def root():
-    return ("Hello world")
+@app.post("/item/")
+async def item():
+    return {"message": "Hello World"}
+
+@app.get("/item/{itemId}")
+async def item_itemId():
+    return {"message": "Hello World"}
+
+@app.delete("/item/{itemId}")
+async def item_Delete():
+    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def items():
+    return {"message": "Hello World"}
--- server/thats_so_server.py
+++ server/thats_so_server.py
@@ -5,4 +5,30 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    return """
+    <html>
+        <head>
+            <title>I'm Sorry I think You're Lost....</title>
+        </head>
+        <body>
+            <h1>I'm Sorry I think You're Lost....</h1><br>
+            <h2>This is not the page you are looking for</h2>
+        </body>
+    </html>
+    """
+
+@app.post("/item/")
+async def item():
+    return {"message": "Hello World"}
+
+@app.get("/item/{itemId}")
+async def item():
+    return {"message": "Hello World"}
+
+@app.delete("/item/{itemId}")
+async def item():
+    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def item():
     return {"message": "Hello World"}
